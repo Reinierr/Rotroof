f1f25275444513dd6b1e3395c743d2396c0fed61
GwnElviin <0907984@hr.nl>
2016-04-11 15:21:26 +0200

    ViewModel Added(Button Style Concept)

--- RotRoof/RotRoof/ChartView.xaml.cs
+++ RotRoof/RotRoof/ChartView.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RotRoof
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChartView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChartView : UserControl
     {
-        public MainWindow()
+        public ChartView()
         {
             InitializeComponent();
-            this.DataContext = new NavigationViewModel();
         }
-
     }
 }
--- RotRoof/RotRoof/MapView.xaml.cs
+++ RotRoof/RotRoof/MapView.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RotRoof
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MapView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MapView : UserControl
     {
-        public MainWindow()
+        public MapView()
         {
             InitializeComponent();
-            this.DataContext = new NavigationViewModel();
         }
-
     }
 }
