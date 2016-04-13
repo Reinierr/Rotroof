c883a72e536fd5e22e0d5c1177af71ad5ac80466
rodofloni <romanr@hotmail.nl>
2016-04-13 14:58:52 +0100

    mal

--- RotRoof/RotRoof/MapView.xaml.cs
+++ RotRoof/RotRoof/MapView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace RotRoof
 {
     /// <summary>
@@ -19,9 +21,25 @@ namespace RotRoof
     /// </summary>
     public partial class MapView : UserControl
     {
+
+        void addNewPolyline2(double x1, double y1, double x2, double y2)
+        {
+            MapPolyline polyline = new MapPolyline();
+            polyline.Stroke = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Blue);
+            polyline.StrokeThickness = 5;
+            polyline.Opacity = 0.7;
+            polyline.Locations = new LocationCollection() {
+            new Location(x1,y1),
+            new Location(x2,y2),};
+            myMap.Children.Add(polyline);
+        }
+
         public MapView()
         {
+
             InitializeComponent();
+            addNewPolyline2(51.917461, 4.483585, 51.917147, 4.483769);
         }
+
     }
 }
